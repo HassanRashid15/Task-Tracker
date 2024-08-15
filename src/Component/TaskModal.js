1bf8ebb3a41f65e6a1766b7256938571bfe1e480
HassanRashid15 <hassanrashid0018@gmail.com>
2024-08-15 12:52:36 -0700

    New Changes Added

--- src/Component/TaskModal.js
+++ src/Component/TaskModal.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
+import { motion } from 'framer-motion';
 
 // Set the root element for accessibility
 Modal.setAppElement('#root');
@@ -15,38 +16,59 @@ function TaskModal({ isOpen, onRequestClose, onAddTask }) {
     }
   };
 
+  const modalVariants = {
+    hidden: { opacity: 0, scale: 0.8 },
+    visible: { opacity: 1, scale: 1, transition: { duration: 0.3 } },
+    exit: { opacity: 0, scale: 0.8, transition: { duration: 0.2 } },
+  };
+
   return (
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
       contentLabel="Add Task Modal"
-      className="flex items-center justify-center"
-      overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
+      style={{
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          transform: 'translate(-50%, -50%)',
+          padding: '20px',
+          width: '300px',
+          borderRadius: '8px',
+        },
+      }}
     >
-      <div className="bg-white rounded-lg p-6 w-80 shadow-lg">
+      <motion.div
+        variants={modalVariants}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+      >
         <h2 className="text-2xl font-semibold mb-4">Add New Task</h2>
         <input
           type="text"
           value={task}
           onChange={(e) => setTask(e.target.value)}
           placeholder="Enter task"
-          className="w-full p-2 mb-4 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full p-2 mb-4 border border-gray-300 rounded "
         />
         <div className="flex justify-end space-x-2">
           <button
             onClick={handleAddTask}
-            className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 "
           >
             Add Task
           </button>
           <button
             onClick={onRequestClose}
-            className="px-4 py-2 bg-gray-300 text-gray-700 rounded hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-500"
+            className="px-4 py-2 bg-gray-300 text-gray-700 rounded hover:bg-gray-400"
           >
             Close
           </button>
         </div>
-      </div>
+      </motion.div>
     </Modal>
   );
 }
--- src/Pages/Tasklist.js
+++ src/Pages/Tasklist.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { MdFilterList } from "react-icons/md";
 import { FaPlus } from "react-icons/fa";
+import { motion, AnimatePresence } from 'framer-motion';
 import TaskModal from './../Component/TaskModal';
 
 function TaskList() {
@@ -9,29 +10,38 @@ function TaskList() {
   const [filter, setFilter] = useState('All');
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  // Load tasks from local storage on component mount
   useEffect(() => {
-    const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    setTasks(storedTasks);
+    try {
+      console.log('Loading tasks from local storage...');
+      const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+      setTasks(storedTasks);
+    } catch (error) {
+      console.error("Failed to load tasks from local storage", error);
+    }
   }, []);
 
-  // Save tasks to local storage whenever tasks change
   useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    try {
+      console.log('Saving tasks to local storage...');
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    } catch (error) {
+      console.error("Failed to save tasks to local storage", error);
+    }
   }, [tasks]);
 
   const addTask = (text) => {
-    const newTask = { id: Date.now(), text, isCompleted: true };
-    setTasks([...tasks, newTask]);
+    const newTask = { id: Date.now(), text, isCompleted: false };
+    setTasks(prevTasks => [...prevTasks, newTask]);
   };
 
   const updateTaskStatus = (id, status) => {
-    setTasks(tasks.map(task =>
-      task.id === id ? { ...task, isCompleted: status === 'Completed' } : task
-    ));
+    setTasks(prevTasks =>
+      prevTasks.map(task =>
+        task.id === id ? { ...task, isCompleted: status === 'Completed' } : task
+      )
+    );
   };
 
-  // Filter tasks based on the selected filter
   const filteredTasks = tasks.filter(task => {
     if (filter === 'All') return true;
     if (filter === 'Completed') return task.isCompleted;
@@ -55,7 +65,7 @@ function TaskList() {
         <div className="md:mt-0 flex items-center">
           <button
             onClick={() => setModalIsOpen(true)}
-            className="flex items-center px-4 py-2 bg-black text-white rounded focus:outline-none focus:ring-2 "
+            className="flex items-center px-4 py-2 bg-black text-white rounded"
           >
             <FaPlus className="lg:mr-2 text-lg" />
             <span className="hidden md:inline">Add Task</span>
@@ -65,33 +75,40 @@ function TaskList() {
       <div className="relative mb-6 flex items-center">
         <button
           onClick={toggleDropdown}
-          className="flex items-center px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-1 "
+          className="flex items-center px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-1"
         >
           <MdFilterList className="text-gray-600" />
           <span className="hidden md:inline ml-2">{filter}</span>
         </button>
-        {isDropdownOpen && (
-          <ul className="absolute top-full bg-white border border-gray-300 rounded shadow-lg z-10">
-            <li
-              className="px-4 py-2 cursor-pointer hover:bg-gray-100"
-              onClick={() => handleFilterChange('All')}
+        <AnimatePresence>
+          {isDropdownOpen && (
+            <motion.ul
+              className="absolute top-full bg-white border border-gray-300 rounded shadow-lg z-10"
+              initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -10 }}
             >
-              All Tasks
-            </li>
-            <li
-              className="px-4 py-2 cursor-pointer hover:bg-gray-100"
-              onClick={() => handleFilterChange('Completed')}
-            >
-              Completed Tasks
-            </li>
-            <li
-              className="px-4 py-2 cursor-pointer hover:bg-gray-100"
-              onClick={() => handleFilterChange('Incomplete')}
-            >
-              Incomplete Tasks
-            </li>
-          </ul>
-        )}
+              <li
+                className="px-4 py-2 cursor-pointer hover:bg-gray-100"
+                onClick={() => handleFilterChange('All')}
+              >
+                All Tasks
+              </li>
+              <li
+                className="px-4 py-2 cursor-pointer hover:bg-gray-100"
+                onClick={() => handleFilterChange('Completed')}
+              >
+                Completed Tasks
+              </li>
+              <li
+                className="px-4 py-2 cursor-pointer hover:bg-gray-100"
+                onClick={() => handleFilterChange('Incomplete')}
+              >
+                Incomplete Tasks
+              </li>
+            </motion.ul>
+          )}
+        </AnimatePresence>
       </div>
       <TaskModal
         isOpen={modalIsOpen}
@@ -100,25 +117,33 @@ function TaskList() {
       />
       {filteredTasks.length > 0 ? (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {filteredTasks.map((task) => (
-            <div key={task.id} className="bg-white border border-gray-300 rounded-lg shadow-md p-4 flex flex-col">
-              <div className={`flex-1 ${task.isCompleted ? 'text-gray-700' : 'text-gray-700 line-through'}`}>
-                {task.text}
-              </div>
-              <button
-                onClick={() => updateTaskStatus(task.id, task.isCompleted ? 'Incomplete' : 'Completed')}
-                className={`mt-4 px-4 py-2 rounded focus:outline-none ${
-                  task.isCompleted ? 'bg-green-500 text-white hover:bg-green-600' : 'bg-red-500 text-white hover:bg-red-600'
-                }`}
+          <AnimatePresence>
+            {filteredTasks.map((task) => (
+              <motion.div
+                key={task.id}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -20 }}
+                className="bg-white border border-gray-300 rounded-lg shadow-md p-4 flex flex-col"
               >
-                {task.isCompleted ? 'Complete' : 'Incomplete'}
-              </button>
-            </div>
-          ))}
+                <div className={`flex-1 ${task.isCompleted ? 'text-gray-700 line-through' : 'text-gray-700'}`}>
+                  {task.text}
+                </div>
+                <button
+                  onClick={() => updateTaskStatus(task.id, task.isCompleted ? 'Incomplete' : 'Completed')}
+                  className={`mt-4 px-4 py-2 rounded focus:outline-none ${
+                    task.isCompleted ? 'bg-red-500 text-white hover:bg-red-600' : 'bg-green-500 text-white hover:bg-green-600'
+                  }`}
+                >
+                  {task.isCompleted ? 'Incomplete' : 'Complete'}
+                </button>
+              </motion.div>
+            ))}
+          </AnimatePresence>
         </div>
       ) : (
         <div className="text-center text-gray-500 py-4">
-          No tasks assigned yet.
+          {filter === 'Completed' ? 'No completed tasks available.' : filter === 'Incomplete' ? 'No incomplete tasks available.' : 'No tasks assigned yet.'}
         </div>
       )}
     </div>
